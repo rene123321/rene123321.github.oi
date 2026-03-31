d80af96114211f1f1ea2a57aa9d41f069baa87f3
rene123321 <reneiiroza@gmail.com>
2026-03-31 03:28:40 -0700

    Update index654767.py

--- index654767.py
+++ index654767.py
@@ -1,1 +1,15 @@
-print("hello world!")
+from flask import Flask, render_template
+
+app = Flask(name)
+
+# Главная страница
+@app.route('/')
+def home():
+    return '<h1>работает!</h1><p>Мой первый сайт на Python</p>'
+
+@app.route('/about')
+def about():
+    return '<h1>Обо мне</h1><p>Я научился делать сайты на Python</p>'
+
+if name == 'main':
+    app.run(debug=True)
